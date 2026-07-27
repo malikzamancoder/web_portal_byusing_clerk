df0f735860523a50a181880d955c618fa71bd1d5
Malik Zaman Khalid <malikzamankhalid12@gmail.com>
2026-07-27 10:04:24 +0500

    console in main

--- authenticationandauthorization/src/main.jsx
+++ authenticationandauthorization/src/main.jsx
@@ -15,6 +15,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY} appearance="dark">
+        console.log("CLERK KEY:", PUBLISHABLE_KEY)
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/admin" element={
